3692f9b27f9b68a69af1ac3d8be258a66f27e8fe
Ramneet-Singh <ramneet2001@gmail.com>
2023-09-10 02:15:14 +0530

    Initial Commit

---
+++ make_autograder.sh
@@ -0,0 +1,1 @@
+zip -r autograder.zip setup.sh run_autograder requirements.txt run_tests.py tests/*.py inputProofFiles/*.txt
---
+++ tests/test_simple.py
@@ -0,0 +1,17 @@
+import unittest
+from gradescope_utils.autograder_utils.decorators import weight, visibility, number
+from checkProof import checkProof
+
+
+class TestComplex(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    @weight(1)
+    @visibility('after_published')
+    @number("1.1")
+    def test_and_intro(self):
+        """Check the handling of the and introduction rule."""
+        proofFilePath = "/autograder/source/inputProofFiles/unit_and_intro.txt"
+        output = checkProof(proofFilePath)
+        self.assertEqual(output, "correct")
